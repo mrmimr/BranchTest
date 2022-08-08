b2691c1c3eda5826fa9736699466dbe15fca4011
mrmimr <alexiaboni7@gmail.com>
2022-08-08 15:01:00 +0200

    created BigProject1 with func #1

---
+++ BranchTest/BigProject1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchTest
+{
+    internal class BigProject1
+    {
+        // Func #1
+    }
+}
